f1ff7872ff2e18edb2291e3e968050f4943dc062
pragyanshu1706 <pragyanshu.gla_cs22@gla.ac.in>
2025-06-17 08:48:44 +0530

    Commit the changes

--- ProgrammingElements/Level_1/ageCalculator.java
+++ ProgrammingElements/Level_1/ageCalculator.java
@@ -1,4 +1,4 @@
-public class ageCalculator{
+public class AgeCalculator{
 	public static void main(String[] args){
 	int birthYear = 2000;
 	System.out.println("Harry's age is in 2024 is " + (2024-birthYear));
